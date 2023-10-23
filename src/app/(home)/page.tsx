4b5a1370ad84b1af5821e0455e7febc15ec2a194
ianabreu <ianabreudev@gmail.com>
2023-10-23 06:21:02 -0300

    feat: add keyboard section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
+import SectionTitle from "./components/section-title";
+import PromoBanner from "./components/promo-banner";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -11,16 +12,20 @@ export default async function Home() {
       },
     },
   });
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
+
   return (
     <div>
       <div className="p-5">
-        <Image
+        <PromoBanner
           src={"/banner-home-01.png"}
-          alt="Até 55% de desconto esse mês!"
-          width={0}
-          height={0}
-          className="h-auto w-full"
-          sizes="100vw"
+          alt={"Até 55% de desconto esse mês!"}
         />
 
         <div className="mt-8">
@@ -29,19 +34,20 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
-        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
+        <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
       <div className="p-5">
-        <Image
+        <PromoBanner
           src={"/banner-home-02.png"}
           alt="Até 55% de desconto em mouses!"
-          width={0}
-          height={0}
-          className="h-auto w-full"
-          sizes="100vw"
         />
+      </div>
+
+      <div className="mt-8">
+        <SectionTitle>Teclados</SectionTitle>
+        <ProductList products={keyboards} />
       </div>
     </div>
   );
