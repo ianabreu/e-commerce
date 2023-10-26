e2c94c1ddad46650474433478caa30daf9d64c8d
ianabreu <ianabreudev@gmail.com>
2023-10-26 18:37:14 -0300

    feat: add link to developer portifolio

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625ram] opacity-75">
-      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    <div className="bg-accent px-8 py-4 text-center text-[0.625ram] opacity-75">
+      <p>
+        © 2023 Copyright <span className="font-semibold">Tech Store</span>
+      </p>
+      <p className="text-sm">
+        Desenvolvido por{" "}
+        <Link
+          target="_blank"
+          className="font-semibold"
+          href={"https://ian-abreu.netlify.app/"}
+        >
+          Ian Abreu
+        </Link>
+      </p>
     </div>
   );
 };
