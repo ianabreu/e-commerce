236b82b84ac0167afee900ee36519d33639034ae
ianabreu <ianabreudev@gmail.com>
2023-12-02 04:12:36 -0300

    feat: add empty cart mensage to cart component

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
+
 const Cart = () => {
   const { products } = useContext(CartContext);
   return (
     <div className="mt-8 flex flex-col gap-5">
-      {products.map((product) => (
-        <CartItem product={product} key={product.slug} />
-      ))}
+      {products.length > 0 ? (
+        products.map((product) => (
+          <CartItem product={product} key={product.slug} />
+        ))
+      ) : (
+        <p className="mb-0 text-center text-xl font-bold">
+          Carrinho vazio. <br />
+          <span className="text-center text-sm font-semibold">
+            Vamos fazer compras?
+          </span>
+        </p>
+      )}
     </div>
   );
 };
